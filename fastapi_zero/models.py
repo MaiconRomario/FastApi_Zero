e363f66f935d7e921a90357b1a6b0fe107a7bbca
MaiconRomario <maiconromario389@hotmail.com>
2025-09-14 01:37:48 -0300

    Sqlalchemy, pytest table User and create session

---
+++ fastapi_zero/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+# Cria o registro
+table_registry = registry()
+
+
+# Usa o decorator para registrar a classe como dataclass e tabela
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    ind: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
